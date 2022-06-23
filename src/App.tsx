afe4e9ea97fedaf341ebcdbdd0f2b228bc54988b
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2022-06-23 18:14:15 -0300

    metade da aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { gql, useQuery } from "@apollo/client"
 import { useEffect } from "react"
+import { Event } from "./Pages/Event";
+
 import { client } from "./lib/apollo"
+import { Sidebar } from "./Components/Sidebar";
 
 const GET_LESSONS_QUERY = gql`
   query {
@@ -24,16 +27,11 @@ interface Lesson {
 
 function App() {
 
-  const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
-
-  console.log(data)
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <>
+      <Event></Event>
+      
+    </>
   )
 }
 
